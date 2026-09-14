ad5511bdbeff59a4e2c89ed6c2d337e8a0c3583d
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:04:49 +0530

    Completed 100 days of C code

--- day32_64.c
+++ day32_64.c
@@ -1,6 +1,30 @@
+/* Q64: Find the digit that occurs the most times in an integer number. */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Day 32 - Problem 64\\n");
+    long long n;
+    if (scanf("%lld", &n) == 1) {
+        int freq[10] = {0}; // Frequency map for digits 0-9
+        n = llabs(n);
+        
+        if (n == 0) {
+            freq[0]++;
+        } else {
+            while (n > 0) {
+                freq[n % 10]++;
+                n /= 10;
+            }
+        }
+        
+        int max_freq = 0, most_frequent_digit = 0;
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > max_freq) {
+                max_freq = freq[i];
+                most_frequent_digit = i;
+            }
+        }
+        printf("Most frequent digit: %d\n", most_frequent_digit);
+    }
     return 0;
 }
