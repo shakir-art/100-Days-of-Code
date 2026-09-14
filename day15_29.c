c4b9f4e8d0137fba6290d329ca15263c2866d111
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:16:55 +0530

    Completed 100 days of C code

--- day15_29.c
+++ day15_29.c
@@ -1,6 +1,18 @@
+/* Q29: Write a program to calculate the factorial of a number. */
 #include <stdio.h>
 
-int main() {
-    printf("Day 15 - Problem 29\\n");
+unsigned long long calculate_factorial(unsigned int n) {
+    unsigned long long fact = 1;
+    for (unsigned int i = 2; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main(void) {
+    unsigned int n;
+    if (scanf("%u", &n) == 1) {
+        printf("Factorial=%llu\n", calculate_factorial(n));
+    }
     return 0;
 }
