d4a759ec1cc01419e589af0a66a5f776a4363387
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:51:15 +0530

    Completed 100 days of C code

--- day23_46.c
+++ day23_46.c
@@ -1,6 +1,12 @@
+/* Q46: Write a program to print a solid 5x5 square pattern using nested loops. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 23 - Problem 46\\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
