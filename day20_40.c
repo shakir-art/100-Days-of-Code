4f230d91ad7a5072feb52973d43d9d7bd00154e1
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:37:58 +0530

    Completed 100 days of C code

--- day20_40.c
+++ day20_40.c
@@ -1,6 +1,18 @@
+/* Q40: Write a program to find the 1's complement of a binary number. */
 #include <stdio.h>
 
+void print_ones_complement(char *binary_str) {
+    printf("1's complement=");
+    for (int i = 0; binary_str[i] != '\0'; i++) {
+        if (binary_str[i] == '0') putchar('1');
+        else if (binary_str[i] == '1') putchar('0');
+        else putchar(binary_str[i]);
+    }
+    putchar('\n');
+}
+
 int main() {
-    printf("Day 20 - Problem 40\\n");
+    char binary[256];
+    if (scanf("%255s", binary) == 1) print_ones_complement(binary);
     return 0;
 }
