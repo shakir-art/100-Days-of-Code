abe322715fdd60016582b46df724b48d84841108
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:00:08 +0530

    Completed 100 days of C code

--- day28_56.c
+++ day28_56.c
@@ -1,6 +1,17 @@
+/* Q56: Read and print elements of a one-dimensional array. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 28 - Problem 56\\n");
+    int n;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
     return 0;
 }
