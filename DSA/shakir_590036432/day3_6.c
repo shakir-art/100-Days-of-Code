f126ba7b77c5b149ec63154f5ad95f560c88c9b4
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 21:40:46 +0530

    Completed 100 days of C code

---
+++ DSA/shakir_590036432/day3_6.c
@@ -0,0 +1,33 @@
+/*
+Q6: Write a program to swap two numbers using a third variable.
+
+Sample Test Cases:
+Input 1:
+3 5
+Output 1:
+After swap: 5 3
+
+Input 2:
+-1 1
+Output 2:
+After swap: 1 -1
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    // Read the two numbers
+    scanf("%d %d", &a, &b);
+
+    // Swap using a third variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    // Print the result in the exact format required
+    printf("After swap: %d %d\n", a, b);
+
+    return 0;
+}
