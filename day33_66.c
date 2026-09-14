dcd9627302f0fa530d4596a39751d106a3267728
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:06:04 +0530

    Completed 100 days of C code

--- day33_66.c
+++ day33_66.c
@@ -1,6 +1,24 @@
+/* Q66: Insert an element in a sorted array at the appropriate position. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 33 - Problem 66\\n");
+    int n, element;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n + 1]; // Allocate extra space for the new element
+        for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+        
+        if (scanf("%d", &element) == 1) {
+            int i;
+            // Shift elements greater than the target to the right
+            for (i = n - 1; (i >= 0 && arr[i] > element); i--) {
+                arr[i + 1] = arr[i];
+            }
+            // Insert element
+            arr[i + 1] = element;
+            
+            for (int j = 0; j <= n; j++) printf("%d ", arr[j]);
+            printf("\n");
+        }
+    }
     return 0;
 }
