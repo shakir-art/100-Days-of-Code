3489e2796ed7f8bc402b0a06f5d0812125bad117
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:33:16 +0530

    Completed 100 days of C code

--- day18_35.c
+++ day18_35.c
@@ -1,6 +1,17 @@
+/* Q35: Write a program to print all factors of a given number. */
 #include <stdio.h>
 
+void print_factors(int n) {
+    if (n <= 0) return;
+    printf("Factors: ");
+    for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0) printf("%d ", i);
+    }
+    printf("%d\n", n);
+}
+
 int main() {
-    printf("Day 18 - Problem 35\\n");
+    int n;
+    if (scanf("%d", &n) == 1) print_factors(n);
     return 0;
 }
