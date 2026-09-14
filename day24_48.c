85abbeb5b6e58023c2dd6424432b6afb22bf8be1
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:53:01 +0530

    Completed 100 days of C code

--- day24_48.c
+++ day24_48.c
@@ -1,6 +1,12 @@
+/* Q48: Write a program to print a right triangle pattern with numbers. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 24 - Problem 48\\n");
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
     return 0;
 }
