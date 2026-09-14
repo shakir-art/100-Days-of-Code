c699f02019d6fb18a2481f3e71a97c20ca0aaf93
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:22:57 +0530

    Completed 100 days of C code

--- day16_31.c
+++ day16_31.c
@@ -1,6 +1,23 @@
+/* Q31: Write a program to print equivalent binary representation. */
 #include <stdio.h>
 
+void print_binary(unsigned int n) {
+    if (n == 0) {
+        printf("0\n");
+        return;
+    }
+    unsigned int binary[64];
+    int index = 0;
+    while (n > 0) {
+        binary[index++] = n % 2;
+        n /= 2;
+    }
+    for (int i = index - 1; i >= 0; i--) printf("%u", binary[i]);
+    printf("\n");
+}
+
 int main() {
-    printf("Day 16 - Problem 31\\n");
+    unsigned int n;
+    if (scanf("%u", &n) == 1) print_binary(n);
     return 0;
 }
