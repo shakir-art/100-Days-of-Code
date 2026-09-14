cd03d27d6435137ecbdbe44f33a9bf6bf379230c
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:02:58 +0530

    Completed 100 days of C code

--- day31_61.c
+++ day31_61.c
@@ -1,6 +1,24 @@
+/* Q61: Search for an element in an array using linear search. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 31 - Problem 61\\n");
+    int n, key, found = -1;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+        
+        if (scanf("%d", &key) == 1) {
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == key) {
+                    found = i;
+                    break;
+                }
+            }
+            if (found != -1) printf("Found at index %d\n", found);
+            else printf("Not found\n");
+        }
+    }
     return 0;
 }
