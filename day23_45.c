fd5e3053bad2880f0afae8ea74214d7fb78a2480
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:46:52 +0530

    Completed 100 days of C code

--- day23_45.c
+++ day23_45.c
@@ -1,6 +1,20 @@
+/* Q45: Find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms. */
 #include <stdio.h>
 
+double compute_series_two(int n) {
+    double sum = 0.0;
+    for (int i = 1; i <= n; i++) {
+        double num = 2.0 * i;
+        double den = (4.0 * i) - 1.0;
+        sum += (num / den);
+    }
+    return sum;
+}
+
 int main() {
-    printf("Day 23 - Problem 45\\n");
+    int n;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        printf("Sum=%.4f\n", compute_series_two(n));
+    }
     return 0;
 }
