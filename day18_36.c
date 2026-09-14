26d555dca1174f89683fecaca5917f5cef072c33
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:33:59 +0530

    Completed 100 days of C code

--- day18_36.c
+++ day18_36.c
@@ -1,6 +1,19 @@
+/* Q36: Write a program to find the HCF (GCD) of two numbers. */
 #include <stdio.h>
 
+int calculate_gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main() {
-    printf("Day 18 - Problem 36\\n");
+    int a, b;
+    if (scanf("%d %d", &a, &b) == 2) {
+        printf("HCF=%d\n", calculate_gcd(a, b));
+    }
     return 0;
 }
