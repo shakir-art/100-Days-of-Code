17265c78de07d93785e691bff054051f7a95ae02
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:34:36 +0530

    Completed 100 days of C code

--- day19_37.c
+++ day19_37.c
@@ -1,6 +1,24 @@
+/* Q37: Write a program to find the LCM of two numbers. */
 #include <stdio.h>
 
+long long calculate_gcd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long long calculate_lcm(long long a, long long b) {
+    if (a == 0 || b == 0) return 0;
+    return (a / calculate_gcd(a, b)) * b;
+}
+
 int main() {
-    printf("Day 19 - Problem 37\\n");
+    long long a, b;
+    if (scanf("%lld %lld", &a, &b) == 2) {
+        printf("LCM=%lld\n", calculate_lcm(a, b));
+    }
     return 0;
 }
