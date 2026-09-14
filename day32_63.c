068709061008581fc70378dbb982a99e7f8ea3c2
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:04:10 +0530

    Completed 100 days of C code

--- day32_63.c
+++ day32_63.c
@@ -1,6 +1,28 @@
+/* Q63: Merge two arrays. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 32 - Problem 63\\n");
+    int n1, n2;
+    
+    // Read first array
+    if (scanf("%d", &n1) == 1 && n1 > 0) {
+        int arr1[n1];
+        for (int i = 0; i < n1; i++) scanf("%d", &arr1[i]);
+        
+        // Read second array
+        if (scanf("%d", &n2) == 1 && n2 > 0) {
+            int arr2[n2];
+            for (int i = 0; i < n2; i++) scanf("%d", &arr2[i]);
+            
+            // Create merged array
+            int merged[n1 + n2];
+            for (int i = 0; i < n1; i++) merged[i] = arr1[i];
+            for (int i = 0; i < n2; i++) merged[n1 + i] = arr2[i];
+            
+            // Print merged array
+            for (int i = 0; i < n1 + n2; i++) printf("%d ", merged[i]);
+            printf("\n");
+        }
+    }
     return 0;
 }
