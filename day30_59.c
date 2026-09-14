641911ba5ce10b51b59165767e348657c9d181ee
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:02:02 +0530

    Completed 100 days of C code

--- day30_59.c
+++ day30_59.c
@@ -1,6 +1,16 @@
+/* Q59: Count even and odd numbers in an array. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 30 - Problem 59\\n");
+    int n, even = 0, odd = 0;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+            if (arr[i] % 2 == 0) even++;
+            else odd++;
+        }
+        printf("Even: %d, Odd: %d\n", even, odd);
+    }
     return 0;
 }
