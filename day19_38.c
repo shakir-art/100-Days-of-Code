a2f66aabb002af18395c81e71b2c236023a554b3
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:35:23 +0530

    Completed 100 days of C code

--- day19_38.c
+++ day19_38.c
@@ -1,6 +1,21 @@
+/* Q38: Write a program to find the sum of digits of a number. */
 #include <stdio.h>
+#include <stdlib.h>
+
+int sum_of_digits(long long n) {
+    int sum = 0;
+    n = llabs(n);
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
 
 int main() {
-    printf("Day 19 - Problem 38\\n");
+    long long n;
+    if (scanf("%lld", &n) == 1) {
+        printf("Sum of digits=%d\n", sum_of_digits(n));
+    }
     return 0;
 }
