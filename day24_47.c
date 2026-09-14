4e823e19cf1b475d97eb125a96dc6cfa21b451c1
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:51:58 +0530

    Completed 100 days of C code

--- day24_47.c
+++ day24_47.c
@@ -1,6 +1,12 @@
+/* Q47: Write a program to print a right triangle pattern with stars. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 24 - Problem 47\\n");
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
