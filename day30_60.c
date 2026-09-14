9261d4bd24e4ed3c94503fb8d7446443f7bd105a
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:02:28 +0530

    Completed 100 days of C code

--- day30_60.c
+++ day30_60.c
@@ -1,6 +1,17 @@
+/* Q60: Count positive, negative, and zero elements in an array. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 30 - Problem 60\\n");
+    int n, pos = 0, neg = 0, zero = 0;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+            if (arr[i] > 0) pos++;
+            else if (arr[i] < 0) neg++;
+            else zero++;
+        }
+        printf("Positive: %d, Negative: %d, Zero: %d\n", pos, neg, zero);
+    }
     return 0;
 }
