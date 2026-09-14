46f7c2eaa8de6fb7837b3f33acd2cbcac434a125
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:00:46 +0530

    Completed 100 days of C code

--- day29_57.c
+++ day29_57.c
@@ -1,6 +1,15 @@
+/* Q57: Find the sum of array elements. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 29 - Problem 57\\n");
+    int n, sum = 0;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+            sum += arr[i];
+        }
+        printf("Sum = %d\n", sum);
+    }
     return 0;
 }
