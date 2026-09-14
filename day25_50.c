51a71068cbfa5142294dabf06d26f64cb7c8dea7
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:55:09 +0530

    Completed 100 days of C code

--- day25_50.c
+++ day25_50.c
@@ -1,6 +1,12 @@
+/* Q50: Write a program to print an inverted right triangle pattern. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 25 - Problem 50\\n");
+    for (int i = 5; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
