35634712eefce5e2ed47fdb89b678ceb1da85268
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:20:42 +0530

    Completed 100 days of C code

--- day15_30.c
+++ day15_30.c
@@ -1,6 +1,19 @@
+/* Q30: Write a program to reverse a given number. */
 #include <stdio.h>
 
+long long reverse_number(long long n) {
+    long long reversed = 0;
+    while (n != 0) {
+        reversed = (reversed * 10) + (n % 10);
+        n /= 10;
+    }
+    return reversed;
+}
+
 int main() {
-    printf("Day 15 - Problem 30\\n");
+    long long n;
+    if (scanf("%lld", &n) == 1) {
+        printf("Reversed=%lld\n", reverse_number(n));
+    }
     return 0;
 }
