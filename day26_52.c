58604bdf714c14068ff557f9617680de3aded2e5
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-14 23:57:26 +0530

    Completed 100 days of C code

--- day26_52.c
+++ day26_52.c
@@ -1,6 +1,16 @@
+/* Q52: Write a program to print a hollow 5x5 square pattern. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 26 - Problem 52\\n");
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (i == 1 || i == 5 || j == 1 || j == 5) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
