cf2963fa9bd1580dc019522f2605084c4f54d711
Shakir <SHAKIR.36432@stu.upes.ac.in>
2026-09-15 00:01:18 +0530

    Completed 100 days of C code

--- day29_58.c
+++ day29_58.c
@@ -1,6 +1,22 @@
+/* Q58: Find the maximum and minimum element in an array. */
 #include <stdio.h>
 
 int main() {
-    printf("Day 29 - Problem 58\\n");
+    int n;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+        
+        int max = arr[0];
+        int min = arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        printf("Max = %d, Min = %d\n", max, min);
+    }
     return 0;
 }
